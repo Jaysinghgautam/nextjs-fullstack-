3ce8272aebb27f3d387f2b97227343629cad3cb5
Jaysinghgautam <gjaysingh53@gmail.com>
2025-08-14 16:04:45 +0530

    updated

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,21 @@
+ import { geDataFromToken } from "@/helpers/getDataFromToken"
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbconfig/dbConfig"
+
+connect();
+
+export async function GET(request:NextRequest){
+
+    try {
+         const userId = await geDataFromToken(request);
+        const user = await User.findOne({_id: userId}).select("-password");
+        return NextResponse.json({
+            mesaaage: "User found",
+            data: user
+        })
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 400});
+    }
+
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -18,20 +18,33 @@ export default function () {
             toast.error(error.message)
         }
     }
+
+    
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id)
+    }
+
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <div>
-            <h1 className="flex justify-center align-middle" >profile</h1>
-            <p className="flex justify-center item-center" >hello  everyone </p>
-        </div> 
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <p>Profile page</p>
+            <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
+            </Link>}</h2>
         <hr />
+        <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >Logout</button>
 
-        <button onClick={logout} className=" flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  " >
-            Logout
-        </button>
+        <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >GetUser Details</button>
 
 
-         </div>
-
+            </div>
     )
 }
